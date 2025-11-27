63c49ad382abaf77f9893750fdf67392fcbda409
kusin <aryajayaalamsyah@gmail.com>
2025-11-28 02:34:13 +0700

    -

--- resources/views/backend/admin/judges/index.blade.php
+++ resources/views/backend/admin/judges/index.blade.php
@@ -44,7 +44,9 @@
                                 <td>
                                     <a href="{{ route('admin.judges.show') }}" class="btn btn-sm btn-outline-info"><i class="fa-solid fa-display"></i></a>
                                     <a href="{{ route('admin.judges.edit') }}" class="btn btn-sm btn-outline-warning"><i class="fa-solid fa-edit"></i></a>
-                                    <a href="" class="btn btn-sm btn-outline-danger"><i class="fa-solid fa-trash"></i></a>
+                                    <button type="button" class="btn btn-sm btn-outline-danger" data-toggle="modal" data-target="#showModal">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
                                 </td>
                                 <td>{{ $item->judges_name ?? '-' }}</td>
                                 <td>{{ $item->origin_country ?? '-' }}</td>
@@ -67,4 +69,62 @@
     {{-- col-sm --}}
 </div>
 {{-- row --}}
+
+<div class="modal fade" id="showModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Are you sure to delete this data ?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="form-group mb-4">
+                            <label for="judges_name">Name of Judges</label>
+                            <input type="text" class="form-control border-dark" name="judges_name" id="judges_name" value="Aryajaya Alamsyah, S.Kom., M.Kom." disabled>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group mb-4">
+                            <label for="origin_country">Origin of Country</label>
+                            <input type="text" class="form-control border-dark" name="origin_country" id="origin_country" value="Indonesia" disabled>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group mb-4">
+                            <label for="origin_institution">Origin of Institution</label>
+                            <input type="text" class="form-control border-dark" name="origin_institution" id="origin_institution" value="Politeknik Negeri Jakarta" disabled>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group mb-4">
+                            <label for="jugdes_task">Assessment Stage</label>
+                            <input type="text" class="form-control border-dark" name="jugdes_task" id="jugdes_task" value="Stage One" disabled>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group mb-4">
+                            <label for="judges_email">Email</label>
+                            <input type="email" class="form-control border-dark" name="judges_email" id="judges_email" value="aryajaya.alamsyah@aseanhub.jakarta.go.id" disabled>
+                        </div>
+                    </div>
+                    {{-- col-sm --}}
+                </div>
+                {{-- row --}}
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-danger">
+                    <i class="fas fa-trash mr-2"></i>Delete
+                </button>
+            </div>
+        </div>
+        {{-- modal-content --}}
+    </div>
+    {{-- modal-dialog --}}
+</div>
+{{-- modal --}}
+
 @endsection
--- resources/views/backend/admin/judges/show.blade.php
+++ resources/views/backend/admin/judges/show.blade.php
@@ -18,11 +18,77 @@
 {{-- row --}}
 
 <div class="row">
-    <div class="col-sm-12">
+    <div class="col-sm-4">
         <div class="card card-info card-outline">
-            <div class="card-header"></div>
-            <div class="card-body"></div>
-            <div class="card-footer"></div>
+            <div class="card-body">
+                <div class="text-center">
+                    <img class="img-fluid img-thumbnail img-circle mb-2"
+                        src="{{ asset('adminlte/dist/img/user8-128x128.jpg') }}"
+                        alt="User profile picture">
+                    <p class="text-normal">Aryajaya Alamsyah, S.Kom., M.Kom.</p>
+                </div>
+                <table class="table border-bottom">
+                    <tbody>
+                        <tr>
+                            <td class="text-left">Status Data</td>
+                            <td class="text-right">
+                                <span class="badge badge-success badge-custom">Active</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-left">Last Updated</td>
+                            <td class="text-right">2026-01-01 - 19:00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        {{-- card --}}
+    </div>
+    <div class="col-sm-8">
+        <div class="card card-info card-outline">
+            <div class="card-header">
+                <h3 class="card-title">Information About the Judges Biodata</h3>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="form-group mb-4">
+                            <label for="judges_name">Name of Judges</label>
+                            <input type="text" class="form-control border-dark" name="judges_name" id="judges_name" value="Aryajaya Alamsyah, S.Kom., M.Kom." disabled>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group mb-4">
+                            <label for="origin_country">Origin of Country</label>
+                            <input type="text" class="form-control border-dark" name="origin_country" id="origin_country" value="Indonesia" disabled>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group mb-4">
+                            <label for="origin_institution">Origin of Institution</label>
+                            <input type="text" class="form-control border-dark" name="origin_institution" id="origin_institution" value="Politeknik Negeri Jakarta" disabled>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group mb-4">
+                            <label for="jugdes_task">Assessment Stage</label>
+                            <input type="text" class="form-control border-dark" name="jugdes_task" id="jugdes_task" value="Stage One" disabled>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group mb-4">
+                            <label for="judges_email">Email</label>
+                            <input type="email" class="form-control border-dark" name="judges_email" id="judges_email" value="aryajaya.alamsyah@aseanhub.jakarta.go.id" disabled>
+                        </div>
+                    </div>
+                    {{-- col-sm --}}
+                </div>
+                {{-- row --}}
+            </div>
         </div>
         {{-- card --}}
     </div>
