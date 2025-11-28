12d03c735a4d002dd762a7e20d55180ef94fc57a
kusin <aryajayaalamsyah@gmail.com>
2025-11-29 00:33:39 +0700

    -

--- resources/views/layouts/frontend_profile.blade.php
+++ resources/views/layouts/frontend_profile.blade.php
@@ -12,7 +12,7 @@
     <link rel="icon" href="{{ asset('adminlte/dist/img/logo-dki.png') }}" type="image/png">
 
     {{-- template style - bootstrap 5.2.x --}}
-    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('bootstrap/bootstrap-5.2.3/dist/css/bootstrap.min.css') }}">
 
     {{-- template style - bootstrap 5.2.x --}}
     <style>
@@ -69,41 +69,29 @@
     </style>
 
     {{-- plugin style - carousel bootstrap 5.2.x --}}
-    <link href="carousel.css" rel="stylesheet">
-
-    {{-- plugin style - other ... --}}
-
+    <link rel="stylesheet" href="{{ asset('bootstrap/carousel.css') }}">
 </head>
 
 <body>
 
-    <header>
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Carousel</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ms-auto mt-2 mb-2">
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Link</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-    </header>
+        </div>
+    </nav>
+    {{-- navbar --}}
 
     <main>
 
@@ -271,10 +259,9 @@
     </main>
 
     {{-- scripts bootstrap 5.2.x --}}
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('bootstrap/bootstrap-5.2.3/dist/js/bootstrap.bundle.min.js') }}"></script>
 
     @stack('scripts')
-
 </body>
 
 </html>
